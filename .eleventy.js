1b1d21528196c3339c426f0445c4ce6e489808ca
Olle Johannesson <olle.johannesson@objectbay.com>
2023-11-02 11:54:24 +0100

    add proper id to movie.data

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const _ = require('lodash');
+const underscoreToDash = str => str.replace(/\_/g, '-')
+const addId = obj => _.set(obj, 'data.id', underscoreToDash(_.get(obj, 'fileSlug')));
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./styles/*.*")
   
   eleventyConfig.addCollection("movies", function(collectionApi) {
+    let c = collectionApi.getFilteredByGlob("movies/*.md").map(addId)
+    console.log(c) 
     return collectionApi.getFilteredByGlob("movies/*.md");
   });
 };
