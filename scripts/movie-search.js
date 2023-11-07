a8a32cda5288b684813b60e8c6a4c1eeb002216b
Olle Johannesson <olle.johannesson@objectbay.com>
2023-11-08 00:19:16 +0100

    filter-by-year function for movie list

--- scripts/movie-search.js
+++ scripts/movie-search.js
@@ -1,5 +1,18 @@
 const filterFunctions = {
-  title: byTitle
+  title: byTitle,
+  year: byReleaseYear
+}
+
+function byTitle(partialTitle) {
+  return element => sanitizeString(element.innerText).includes(sanitizeString(partialTitle))
+}
+
+function byReleaseYear(earliestReleaseYear) {
+  return element => parseInt(element.dataset.year) > earliestReleaseYear
+}
+
+function sanitizeString(string) {
+  return string.normalize('NFC').trim().toLowerCase();
 }
 
 function getAllMovies() {
@@ -10,17 +23,22 @@ function getSearchForm() {
   return document.getElementById('search')
 }
 
+function getYearSlider() {
+  return document.getElementById('year')
+}
+
+function initYearSlider() {
+  const slider = getYearSlider()
+  slider.max = (new Date()).getFullYear()
+  slider.value = slider.min
+  const updateValue = value => document.getElementById('year-value').textContent = value
+  updateValue(slider.min)
+  slider.addEventListener('input', e => updateValue(e.target.value))
+}
+
 function getFormDataEntries() {
   const formData = new FormData(getSearchForm());
   return [...formData.entries()]
-}
-
-function byTitle(partialTitle) {
-  return element => sanitizeString(element.innerText).includes(sanitizeString(partialTitle))
-}
-
-function sanitizeString(string) {
-  return string.normalize('NFC').trim().toLowerCase();
 }
 
 function searchFormEntryToPredicate([filterName, filterValue]) {
@@ -42,4 +60,5 @@ function filterMovies() {
   getAllMovies().forEach(showElementIf(allFiltersApply))
 }
 
+initYearSlider()
 getSearchForm().addEventListener('input', filterMovies)
